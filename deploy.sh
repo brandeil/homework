5b77db68daea50786c43b32e5f81e3883a128b11
LB <me@gmail.com>
2018-06-15 20:42:53 -0400

    frame the bash script with comments

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# connect to AWS CLI to create the stack
+
+# remote into the EC2 and use sed to update ~/airflow/airflow.cfg to values from AWS resources
+#sql_alchemy_conn=
+#base_url=
+#web_server_host
+#web_server_port
+#broker_url
+#celery_result_backend
 
 
+
+# initialize the database
+airflow initdb
+
+# start the web server, default port is 8080
+airflow webserver -p 8080
 
